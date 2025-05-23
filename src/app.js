d4c881115d5fb3dd19cce6ecaa28ef14b7acb4da
Rokkam <rokkamanu542@gmail.com>
2025-05-23 11:37:04 +0530

    Devtinder

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("This is the home page");
+
+})
+app.get("/hello",(req,res)=>{
+    res.send("This is the hello page");
+
+})
+app.listen(3000,()=>{
+    console.log("app is running succsefully");
+})
